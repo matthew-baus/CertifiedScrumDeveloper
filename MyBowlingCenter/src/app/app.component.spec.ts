c77e58384f8cc6835de78bd8494a8e6cdda6c975
Matthew Baus <matthewbaus@Matthews-MacBook-Pro.local>
2020-06-26 15:47:18 -0500

    Entering Form

--- MyBowlingCenter/src/app/app.component.spec.ts
+++ MyBowlingCenter/src/app/app.component.spec.ts
@@ -32,4 +32,6 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('MyBowlingCenter app is running!');
   });
+
+  
 });
