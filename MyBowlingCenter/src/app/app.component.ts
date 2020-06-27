71d141a83c73e2e0eaf6d709dc0ce73fe13c8cd7
Matthew Baus <matthewbaus@Matthews-MacBook-Pro.local>
2020-06-27 14:54:47 -0500

    With tests

--- MyBowlingCenter/src/app/app.component.ts
+++ MyBowlingCenter/src/app/app.component.ts
@@ -7,19 +7,12 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   counter = 0;
-  total = 0;
-  score = 0;
 
   increment(): void {
     this.counter++;
   }
 
   decrement(): void {
-    this.counter--;
-  }
-
-  enter(score): void {
-    this.score = score;
-    this.total += this.score;
+    this.counter--
   }
 }
